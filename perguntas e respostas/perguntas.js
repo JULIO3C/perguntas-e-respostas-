a889b5c4d934d1ec86873457a1fc045fd7b20c81
JULIO3C <00001083283273sp@al.educacao.sp.gov.br>
2024-11-28 10:15:39 -0300

    Add files via upload

---
+++ perguntas e respostas/perguntas.js
@@ -0,0 +1,8 @@
+criaCartao( 'Química', 'O que é Química?', 'Química é a ciência que estuda a composição, propriedades e reações da matéria.' )
+
+ criaCartao( 'Química', 'Quais são os principais ramos da Química?', 'Química Orgânica, Química Inorgânica, Química Física, Química Analítica e Bioquímica.' )
+
+ criaCartao( 'Química', 'Qual é a diferença entre átomo e molécula?', 'Átomo é a unidade básica da matéria, enquanto molécula é um grupo de átomos unidos.' )
+ criaCartao( 'Química', 'O que é reação química?', 'Reação química é um processo em que substâncias são transformadas em outras substâncias.' )
+
+ criaCartao( 'Química', 'Qual é a Lei da Conservação da Massa?', 'A massa total dos reagentes é igual à massa total dos produtos.' )
